7ab7168acc1e714e17792e93ce9afba11ae5de61
Vishak <wizvishak@gmail.com>
2016-07-01 02:39:32 +0530

    [ADD] Blockquotes
    [ADD] Blockquotes & examples

---
+++ app/controllers/index.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	quotables: [{
+		text: 'Compassion emerges from imagining the world alive.',
+		author: 'Alexandra Horowitz'
+	}, {
+		text: 'Knowing more and more … can block the passageways to feeling.',
+		author: 'Elizabeth Alexander'
+	}]
+});
