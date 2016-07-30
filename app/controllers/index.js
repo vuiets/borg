6f69cb285a2ac778ac5d81ff21fec0b0e600f36e
Vishak <wizvishak@gmail.com>
2016-07-30 21:11:26 +0530

    [UPDATE] ember-cli, tachyons.
    [ADD] e-sctn component

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,7 +5,13 @@ export default Ember.Controller.extend({
 		text: 'Compassion emerges from imagining the world alive.',
 		author: 'Alexandra Horowitz'
 	}, {
-		text: 'Knowing more and more … can block the passageways to feeling.',
-		author: 'Elizabeth Alexander'
+		text: 'I spend too much time trying to spend less time.',
+		author: 'Michael Lewis'
+	}, {
+		text: 'Everything has the potential to inspire.',
+		author: 'Jonathan Safran Foer'
+	}, {
+		text: 'We will never have a perfect world, but it\'s not romantic or naive to work toward a better one.',
+		author: 'Steven Pinker'
 	}]
 });
