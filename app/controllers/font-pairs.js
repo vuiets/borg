7e9572690afdf0f0649ba75ffa53d059ee00aea8
Visahavel <wizvishak@gmail.com>
2016-10-04 23:48:40 +0530

    new routes (#12)

---
+++ app/controllers/font-pairs.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	quotables: [{
+		text: 'Compassion emerges from imagining the world alive.',
+		author: 'Alexandra Horowitz'
+	}, {
+		text: 'I spend too much time trying to spend less time.',
+		author: 'Michael Lewis'
+	}, {
+		text: 'Everything has the potential to inspire.',
+		author: 'Jonathan Safran Foer'
+	}, {
+		text: 'We will never have a perfect world, but it\'s not romantic or naive to work toward a better one.',
+		author: 'Steven Pinker'
+	}]
+});
